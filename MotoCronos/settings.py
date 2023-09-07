eb0cf7e41b953c7fbd8f2266685fe50c47db9bb8
matiasyurquina <52612811+matiasyurquina@users.noreply.github.com>
2023-09-07 04:39:19 +0400

    Update settings.py
    connection

--- MotoCronos/settings.py
+++ MotoCronos/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'MotoCronos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mc',
+        'USER' : 'matias',
+        'PASSWORD' : '123456',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
